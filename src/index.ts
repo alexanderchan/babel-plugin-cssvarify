68b44a67702f48afde626a64263fb3e8044ae72c
Alex Chan <alexander.m.chan@gmail.com>
2021-05-08 10:31:12 -0600

    add template plugin

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+const replaceToken = '--'
+
+const varReplaceRegex = new RegExp(
+  `(?<!var\\(\\s*)\\${replaceToken}([a-zA-Z\\-_\\d]+)`,
+  'gm'
+)
+
+function addVar(cssPropertyValue) {
+  if (typeof cssPropertyValue !== 'string') {
+    return cssPropertyValue
+  }
+
+  return cssPropertyValue.replace(varReplaceRegex, 'var(--$1)')
+}
+
+export function traverseVars({ t }) {
+  return {
+    StringLiteral(path) {
+      path.node.value = addVar(path.node.value)
+    },
+    TemplateElement(path) {
+      path.node.value.raw = addVar(path.node.value.raw)
+      path.node.value.cooked = addVar(path.node.value.cooked)
+    },
+  }
+}
+
+export default function ({ types: t }) {
+  return {
+    name: '@alexmchan/cssvarify',
+    visitor: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      JSXAttribute(path) {
+        if (['style'].includes(path.node.name.name)) {
+          path.traverse(traverseVars({ t }))
+        }
+      },
+    },
+  }
+}
